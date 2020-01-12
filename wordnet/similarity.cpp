f7e699f8590aa427598140af5089e4241942b8f1
Carmine Margiotta <car.margiotta@icloud.com>
2020-01-12 02:39:05 +0100

    Trovata l'intersezione tra le derivazioni di due parole

--- wordnet/similarity.cpp
+++ wordnet/similarity.cpp
@@ -1,29 +1,50 @@
-#include <algorithm>
-
 #include "similarity.hpp"
 
-static bool similarity::is_sentence(const std::string& string)
-{
-	//A string without spaces is a word
-	return std::find(string.begin(), string.end(), ' ') != string.end();
-}
+#include <vector>
+#include <list>
+#include <utility>
+#include <algorithm>
+#include <iostream>
 
-static float similarity::compare(const std::string& string1, const std::string& string2)
+#include "wordnet.hpp"
+
+using namespace std;
+
+float similarity::compare_words(unsigned int word1, unsigned int word2)
 {
-	if (is_sentence(string1) || is_sentence(string2)
+	static wordnet wn("wordnet/dict/");
+	
+	tree<unsigned int> path1 = wn.hypernym_tree(word1);
+	tree<unsigned int> path2 = wn.hypernym_tree(word2);
+	
+	for (auto i: path1.path)
+		cout << wn.get_word(i.first) << "   " << i.second << endl;
+		cout << endl;cout << endl;
+	for (auto i: path2.path)
+		cout << wn.get_word(i.first) << "   " << i.second << endl;
+		cout << endl;cout << endl;
+	
+	std::map<unsigned int, unsigned int> intersection;
+	std::set_intersection  (path1.path.begin(), 
+							path1.path.end(), 
+							path2.path.begin(), 
+							path2.path.end(), 
+							inserter (intersection, intersection.begin()),
+		[](auto& p1, auto& p2)
+        {
+			 return p1.first < p2.first;
+		});
+		
+	auto best_common_parent = *min_element(intersection.begin(), intersection.end(), [&path1, &path2](auto el1, auto el2)
 	{
-		return compare_sentences(string1, string2);
-	}
+		return (path1.path[el1.first]+path2.path[el1.first]) < (path1.path[el2.first]+path2.path[el2.first]);
+	});
 	
-	return compare_words(string1, string2);
-}
-
-static float similarity::compare_words(const std::string& word1, const std::string& word2)
-{
+	for (auto i: intersection)
+		cout << wn.get_word(i.first) << endl;
+	cout << endl;
 	
-}
-
-static float similarity::compare_sentences(const std::string& sentence1, const std::string& sentence2)
-{
+	cout << wn.get_word(best_common_parent.first) << endl;
 	
+	return 0.0f;
 }
