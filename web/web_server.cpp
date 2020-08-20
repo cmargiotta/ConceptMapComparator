f16e86c668db920dea2d6ba53e82c4ff60f14a3e
Carmine Margiotta <car.margiotta@icloud.com>
2020-08-20 17:05:27 +0200

    Iniziato lo sviluppo del frontend

--- web/web_server.cpp
+++ web/web_server.cpp
@@ -1,1 +1,28 @@
+#include <string>
+#include <fstream>
+#include <streambuf>
+#include <iostream>
 
+#include "lithium_http_backend.hh"
+
+using namespace li;
+using namespace std;
+
+int main() {
+	// Reading index.html
+	ifstream t("./index.html");
+	stringstream buffer;
+	buffer << t.rdbuf();
+	string body = buffer.str();
+	
+	cout << body;
+	
+	http_api api;
+
+	api.get("") = [&](http_request&, http_response& response) {
+		cout << body;
+		response.write(body);
+	};
+
+	http_serve(api, 8080);
+}
