b30de6e4ddf05e67844e84314102c9d184969bd3
Carmine Margiotta <car.margiotta@icloud.com>
2020-08-28 13:10:14 +0200

    Ora il web server confronta le mappe ricevute e invia il risultato del confronto come risposta alla richiesta POST

--- web/web_server.cpp
+++ web/web_server.cpp
@@ -5,12 +5,14 @@
 
 #include <httplib.h>
 
+#include <concept_map.hpp>
+
 using namespace httplib;
 using namespace std;
 
 int main() {
 	// Reading index.html
-	ifstream t("./index.html");
+	ifstream t("web/index.html");
 	stringstream buffer;
 	buffer << t.rdbuf();
 	string body = buffer.str();
@@ -22,15 +24,26 @@ int main() {
 	});
 	
 	svr.Post("/upload", [&](const auto& req, auto& res) {
-		auto size = req.files.size();
-
 		const auto& map1 = req.get_file_value("map1");
 		const auto& map2 = req.get_file_value("map2");
 		// file.filename;
 		// file.content_type;
 		// file.content;
+		cout << "Maps received\n";
 		
-		res.set_content(map1.content, "text/plain");
+		try {
+			stringstream ss1 = stringstream(map1.content);
+			stringstream ss2 = stringstream(map2.content);
+			concept_map cm1 (ss1);
+			concept_map cm2 (ss2);
+			
+			cout << "Starting comparison\n";
+			
+			res.set_content(to_string(cm1.similarity(cm2)), "text/plain");
+		} catch(std::runtime_error e)
+		{
+			res.set_content(e.what(), "text/plain");
+		}
 	});
 
 	svr.listen("localhost", 8080);
